7eb585ff61a4c3b5d030956b06ad1010478366e4
Анастасия <goodmorning.25@mail.ru>
2020-12-14 15:10:34 +0300

    Работа над адаптивностью главной страницы

--- source/js/script.js
+++ source/js/script.js
@@ -2,3 +2,10 @@ function Click() {
   document.getElementById('scr').src = (document.getElementById('typ').checked)?
   'img/mobile-after.png' : 'img/mobile-before.png'
 }
+
+const slider = document.getElementById('slider');
+const beforeImage = document.getElementById('before');
+
+slider.addEventListener('input', function() {
+  beforeImage.style.width = slider.value + '%';
+});
